91f60fa5d3136ce95a636f0a7194c5a0c3242d2e
hulilei <huuuu1016@gmail.com>
2024-09-26 11:48:51 +0800

    iOS demo 修复配置拷贝解析异常崩溃问题

--- src/ios/demo/GuanceDemo/View/Helpers/StringExtension.swift
+++ src/ios/demo/GuanceDemo/View/Helpers/StringExtension.swift
@@ -56,13 +56,13 @@ struct Configuration:Codable,Hashable{
 protocol PasteConfiguration {
     func parsePaste() throws -> Configuration
     
-    func base64Decoding()->String
+    func base64Decoding() throws ->String
 }
 extension String:PasteConfiguration {
     func parsePaste() throws -> Configuration {
         if self.hasPrefix(configPrefix) {
             let str = String(self.dropFirst(configPrefix.count))
-            let decodeStr = str.base64Decoding()
+            let decodeStr = try str.base64Decoding()
             if let jsonData = decodeStr.data(using: .utf8) {
                 let decoder = JSONDecoder()
                 do {
@@ -79,9 +79,12 @@ extension String:PasteConfiguration {
         throw PasteConfigurationError.formatError
     }
     
-    func base64Decoding()->String{
+    func base64Decoding()throws ->String{
         let decodeData = NSData(base64Encoded: self,options: NSData.Base64DecodingOptions.init(rawValue: 0))
-        let decodeStr = NSString(data: decodeData! as Data, encoding: NSUTF8StringEncoding)! as String
-        return decodeStr
+        if let data = decodeData {
+            let decodeStr = NSString(data: data as Data, encoding: NSUTF8StringEncoding)! as String
+            return decodeStr
+        }
+        throw PasteConfigurationError.parseError
     }
 }
